97df44ef15083f5b08195ca87ed2438d65347874
Josh Cheek <josh.cheek@gmail.com>
2014-08-31 23:52:30 -0600

    Have spec delegate get request to the helper method

--- spec/eval_in_spec.rb
+++ spec/eval_in_spec.rb
@@ -53,8 +53,10 @@ RSpec.describe EvalIn, integration: true do
     # iffy solution, but it's simple and works,
     # Rexml might get taken out of stdlib, so is more likely than this regex to fail in the future,
     # and I don't want to add dep on Nokogiri (w/ libxml & libxslt) where a small regex works adequately
-    body = Net::HTTP.start('eval.in', 443, use_ssl: true) { |http| http.request_get('/').body }
-    current_known_languages = body.each_line.map { |line| line[/option.*?value="([^"]+)"/, 1] }.compact
+    current_known_languages = EvalIn.get_request('https://eval.in')
+                                    .each_line
+                                    .map { |line| line[/option.*?value="([^"]+)"/, 1] }
+                                    .compact
     expect(EvalIn::KNOWN_LANGUAGES).to eq current_known_languages
   end
 end
