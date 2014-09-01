0d5a50d86cb02f5cc721be7bb387afbfbb238044
Josh Cheek <josh.cheek@gmail.com>
2014-09-01 00:34:44 -0600

    Extract common functionality in get and post into generic request
    ...it occurs to me that I don't know the difference between a URL and a URI
    
    UPDATE:
    
    Went and read this http://stackoverflow.com/questions/176264/whats-the-difference-between-a-uri-and-a-url/1984225#1984225
    ...still have no fucking clue
    
    Found my way to this http://www.ietf.org/rfc/rfc3986.txt
    ...and decided I didn't actually care that much
    
    Why is it that hard to say "here is the full set of possible
    information you can express with these things, and here are the
    mandatory and optional ones for a URL and a URI"?
    
    Whatevez.

--- lib/eval_in.rb
+++ lib/eval_in.rb
@@ -147,37 +147,42 @@ module EvalIn
     uri.to_s
   end
 
-  # @api private
-  # Can't just use Net::HTTP.get, b/c it doesn't use ssl on 1.9.3
-  # https://github.com/ruby/ruby/blob/v2_1_2/lib/net/http.rb#L478-479
-  # https://github.com/ruby/ruby/blob/v1_9_3_547/lib/net/http.rb#L454
-  def self.get_request(raw_url, user_agent)
-    uri                   = URI raw_url
-    path                  = uri.path
-    path                  = '/' if path.empty?
-    request               = Net::HTTP::Get.new(path)
-    request['User-Agent'] = user_agent
-    Net::HTTP.start(uri.hostname, uri.port, use_ssl: (uri.scheme == 'https')) { |http| http.request request }
-  end
-
-  # @private
-  # stole this out of implementation for post_form https://github.com/ruby/ruby/blob/2afed6eceff2951b949db7ded8167a75b431bad6/lib/net/http.rb#L503
-  # can use this to view the request: `net.set_debug_output $stdout` (pretty sure this obj is the http param in the block)
-  def self.post_request(raw_url, form_data, user_agent)
-    uri                   = URI raw_url
-    path                  = uri.path
-    path                  = '/' if path.empty?
-    request               = Net::HTTP::Post.new(path)
-    request.form_data     = form_data
-    request['User-Agent'] = user_agent
-    request.basic_auth uri.user, uri.password if uri.user
-    Net::HTTP.start(uri.hostname, uri.port, use_ssl: (uri.scheme == 'https')) { |http| http.request request }
-  end
-
   # @private
   def self.user_agent_for(context)
     'http://rubygems.org/gems/eval_in'.tap do |agent|
       context && agent.concat(" (#{context})")
     end
+  end
+
+  # @api private
+  # Can't just use Net::HTTP.get, b/c it doesn't use ssl on 1.9.3
+  # https://github.com/ruby/ruby/blob/v2_1_2/lib/net/http.rb#L478-479
+  # https://github.com/ruby/ruby/blob/v1_9_3_547/lib/net/http.rb#L454
+  def self.get_request(raw_url, user_agent)
+    generic_request_for raw_url:      raw_url,
+                        request_type: Net::HTTP::Get,
+                        user_agent:   user_agent
+  end
+
+  # @private
+  def self.post_request(raw_url, form_data, user_agent)
+    generic_request_for raw_url:      raw_url,
+                        request_type: Net::HTTP::Post,
+                        user_agent:   user_agent,
+                        form_data:    form_data
+  end
+
+  # @private
+  # stole this out of implementation for post_form https://github.com/ruby/ruby/blob/2afed6eceff2951b949db7ded8167a75b431bad6/lib/net/http.rb#L503
+  # can use this to view the request: http.set_debug_output $stdout
+  def self.generic_request_for(params)
+    uri                   = URI params.fetch(:raw_url)
+    path                  = uri.path
+    path                  = '/' if path.empty?
+    request               = params.fetch(:request_type).new(path)
+    request['User-Agent'] = params[:user_agent] if params.key? :user_agent
+    request.form_data     = params[:form_data]  if params.key? :form_data
+    request.basic_auth uri.user, uri.password   if uri.user
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: (uri.scheme == 'https')) { |http| http.request request }
   end
 end
