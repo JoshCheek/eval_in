5e8c522e3dbd664de8af5117a87ab89f343af17f
Josh Cheek <josh.cheek@gmail.com>
2014-10-19 16:57:08 -0600

    Fix bug in test from making the methods actually private
    ...yeah, I think I actually want to extract these methods into classes

--- spec/eval_in_spec.rb
+++ spec/eval_in_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe EvalIn do
       # iffy solution, but it's simple and works,
       # Rexml might get taken out of stdlib, so is more likely than this regex to fail in the future,
       # and I don't want to add dep on Nokogiri (w/ libxml & libxslt) where a small regex works adequately
-      current_known_languages = EvalIn.get_request('https://eval.in', context)
+      current_known_languages = EvalIn.__send__(:get_request, 'https://eval.in', context)
                                       .body
                                       .each_line
                                       .map { |line| line[/option.*?value="([^"]+)"/, 1] }
