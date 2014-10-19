6e61032b31489ac3c98e2ce8f2923c33942eaa0d
Josh Cheek <josh.cheek@gmail.com>
2014-10-19 16:56:01 -0600

    Normalize the call body to use the on_call callback

--- lib/eval_in/mock.rb
+++ lib/eval_in/mock.rb
@@ -6,20 +6,31 @@ module EvalIn
     def initialize(options={})
       @result          = options.fetch :result,          nil
       @languages       = options.fetch :languages,       {}
-      @on_call         = options.fetch :on_call,         nil
+      @on_call         = options.fetch(:on_call)         { method :evaluate_with_tempfile }
       @on_fetch_result = options.fetch(:on_fetch_result) { EvalIn.method :fetch_result }
     end
 
     def call(code, options={})
       language_name = EvalIn.__send__ :language_or_error_from, options
-      return @result                      if @result
-      return @on_call.call(code, options) if @on_call
-      tempfile = Tempfile.new 'EvalIn-mock'
+      return @result if @result
+      @on_call.call(code, options)
+    end
+
+    def fetch_result(raw_url, options={})
+      return @result if @result
+      @on_fetch_result.call(raw_url, options)
+    end
+
+    private
+
+    def evaluate_with_tempfile(code, options={})
+      language_name = EvalIn.__send__ :language_or_error_from, options
+      tempfile      = Tempfile.new 'EvalIn-mock'
       tempfile.write code
       tempfile.close
-      lang    = @languages.fetch language_name
-      program = lang.fetch(:program)
-      args    = lang.fetch(:args, []) + [tempfile.path]
+      lang          = @languages.fetch language_name
+      program       = lang.fetch(:program)
+      args          = lang.fetch(:args, []) + [tempfile.path]
       out, status = Open3.capture2e(program, *args)
       Result.new output:            out,
                  exitstatus:        status.exitstatus,
@@ -30,11 +41,6 @@ module EvalIn
                  status:            'OK (0.072 sec real, 0.085 sec wall, 8 MB, 19 syscalls)'
     ensure
       tempfile.unlink if tempfile
-    end
-
-    def fetch_result(raw_url, options={})
-      return @result if @result
-      @on_fetch_result.call(raw_url, options)
     end
   end
 end
