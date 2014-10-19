9c3573eaa19e78d9659d602af3d883dd03df6fc2
Josh Cheek <josh.cheek@gmail.com>
2014-10-19 14:16:39 -0600

    Extract result into its own file

--- lib/eval_in.rb
+++ lib/eval_in.rb
@@ -4,6 +4,7 @@ require 'uri'
 require 'json'
 require 'net/http'
 require 'pathname'
+require 'eval_in/result'
 require 'eval_in/version'
 
 module EvalIn
@@ -40,40 +41,6 @@ module EvalIn
     slash/slash-head
     assembly/nasm-2.07
   ]
-
-  # The data structure containing the final result
-  # its attributes default to null-objects for their given type
-  class Result
-    @attribute_names = [:exitstatus, :language, :language_friendly, :code, :output, :status, :url].freeze
-    attr_accessor *@attribute_names
-    class << self
-      attr_reader :attribute_names
-    end
-
-    def initialize(attributes={})
-      attributes = attributes.dup
-      self.exitstatus         = attributes.delete(:exitstatus)        || -1
-      self.language           = attributes.delete(:language)          || ""
-      self.language_friendly  = attributes.delete(:language_friendly) || ""
-      self.code               = attributes.delete(:code)              || ""
-      self.output             = attributes.delete(:output)            || ""
-      self.status             = attributes.delete(:status)            || ""
-      self.url                = attributes.delete(:url)               || ""
-      stderr                  = attributes.delete(:stderr)            || $stderr
-      stderr.puts "Unexpected attributes! #{attributes.keys.inspect}" if attributes.any?
-    end
-
-    # Returns representation of the result built out of JSON primitives (hash, string, int)
-    def as_json
-      self.class.attribute_names.each_with_object Hash.new do |name, attributes|
-        attributes[name.to_s] = public_send name
-      end
-    end
-
-    def to_json
-      JSON.dump(as_json)
-    end
-  end
 
   # @param code [String] the code to evaluate.
   # @option options [String] :language Mandatory, a language recognized by eval.in, such as any value in {KNOWN_LANGUAGES}.
---
+++ lib/eval_in/result.rb
@@ -0,0 +1,35 @@
+module EvalIn
+  # The data structure containing the final result
+  # its attributes default to null-objects for their given type
+  class Result
+    @attribute_names = [:exitstatus, :language, :language_friendly, :code, :output, :status, :url].freeze
+    attr_accessor *@attribute_names
+    class << self
+      attr_reader :attribute_names
+    end
+
+    def initialize(attributes={})
+      attributes = attributes.dup
+      self.exitstatus         = attributes.delete(:exitstatus)        || -1
+      self.language           = attributes.delete(:language)          || ""
+      self.language_friendly  = attributes.delete(:language_friendly) || ""
+      self.code               = attributes.delete(:code)              || ""
+      self.output             = attributes.delete(:output)            || ""
+      self.status             = attributes.delete(:status)            || ""
+      self.url                = attributes.delete(:url)               || ""
+      stderr                  = attributes.delete(:stderr)            || $stderr
+      stderr.puts "Unexpected attributes! #{attributes.keys.inspect}" if attributes.any?
+    end
+
+    # Returns representation of the result built out of JSON primitives (hash, string, int)
+    def as_json
+      self.class.attribute_names.each_with_object Hash.new do |name, attributes|
+        attributes[name.to_s] = public_send name
+      end
+    end
+
+    def to_json
+      JSON.dump(as_json)
+    end
+  end
+end
