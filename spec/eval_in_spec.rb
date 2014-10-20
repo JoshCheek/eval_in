c15f4dbc007d17ba7a0a9670b39c1473778ca7b0
Josh Cheek <josh.cheek@gmail.com>
2014-10-20 12:17:49 -0600

    Make a non-integration test for EvalIn.call
    Just so I can see if it works without having to have internet
    or hit the real server. And because Travis doesn't run the
    integration specs (idk if there's established protocol around
    things like that or not), so I just want a little more immediate
    feedback.

--- spec/eval_in_spec.rb
+++ spec/eval_in_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe EvalIn do
 
     let(:context) { 'eval_in integration test' }
 
-    it '.call evaluates Ruby code through eval.in' do
+    example 'using .call to evaluate Ruby code' do
       result = EvalIn.call 'print "hello, #{gets}"', stdin: "world", language: "ruby/mri-2.1", context: context
       expect(result.exitstatus       ).to eq 0
       expect(result.language         ).to eq "ruby/mri-2.1"
@@ -23,7 +23,7 @@ RSpec.describe EvalIn do
       expect(result.url              ).to match %r(https://eval.in/\d+.json)
     end
 
-    it '.fetch_result fetches previous results from eval.in' do
+    example 'using .fetch_result to fetch a known result from eval.in' do
       result = EvalIn.fetch_result "https://eval.in/147.json"
       expect(result.exitstatus       ).to eq 0
       expect(result.language         ).to eq "ruby/mri-1.9.3"
@@ -34,7 +34,7 @@ RSpec.describe EvalIn do
       expect(result.url              ).to match %r(https://eval.in/147.json)
     end
 
-    it 'is in sync with known languages' do
+    example 'ensure the KNOWN_LANGUAGES are up to date' do
       # iffy solution, but it's simple and works,
       # Rexml might get taken out of stdlib, so is more likely than this regex to fail in the future,
       # and I don't want to add dep on Nokogiri (w/ libxml & libxslt) where a small regex works adequately
@@ -48,8 +48,12 @@ RSpec.describe EvalIn do
   end
 
 
-  describe '.fetch_result' do
+
+  describe 'toplevel methods' do
     include WebMock::API
+
+    let(:ruby_result) { {'lang' => 'some lang', 'lang_friendly' => 'some lang friendly', 'code' => 'some code', 'output' => 'some output', 'status' => 'some status'} }
+    let(:json_result) { JSON.dump ruby_result }
 
     def stub_eval_in(url, options={})
       stub_request(:get, url)
@@ -57,32 +61,58 @@ RSpec.describe EvalIn do
         .to_return(status: 200, body: json_result)
     end
 
-    let(:ruby_result) { {'lang' => 'some lang', 'lang_friendly' => 'some lang friendly', 'code' => 'some code', 'output' => 'some output', 'status' => 'some status'} }
-    let(:json_result) { JSON.dump ruby_result }
+    describe '.call' do
+      it 'posts code through eval.in, following the redirect to return an EvalIn::Result' do
+        post_url   = 'http://example.com/fake_post_endpoint'
+        get_url    = 'http://example.com/fake_get_endpoint.json'
+        context    = 'SOME CONTEXT'
+        user_agent = "http://rubygems.org/gems/eval_in (#{context})"
 
-    it 'wraps fetch_result_json and build_result' do
-      url = 'https://eval.in/1.json'
-      stub_eval_in url
-      result = EvalIn.fetch_result url
-      assert_result result,
-                    exitstatus:        0,
-                    language:          ruby_result['lang'],
-                    language_friendly: ruby_result['lang_friendly'],
-                    code:              ruby_result['code'],
-                    output:            ruby_result['output'],
-                    status:            ruby_result['status'],
-                    url:               url
+        # stub the post, verifies the context and url got passed through, redirects to the get
+        stub_request(:post, post_url)
+          .with(headers: {'User-Agent' => user_agent})
+          .to_return(status: 302, headers: {'location' => get_url}, body: json_result)
+
+        # stub the get to return the mock result
+        stub_eval_in(get_url, user_agent: user_agent) 
+
+        # make the request, posting the code, following the redirect, parsing the json, turning it into a Result
+        result = EvalIn.call 'print "hello, #{gets}"', language: "ruby/mri-2.1", context: context, url: post_url
+
+        # Just verify that one of the expected values is there,
+        # all the specifics are better tested in .fetch_result and the integration tests
+        # this is just a quick wiring test with a "data looks about right"
+        expect(result.language).to eq ruby_result['lang']
+      end
     end
 
-    it 'jsonifies the url if it isn\'t already' do
-      stub_eval_in 'https://eval.in/1.json'
-      expect(EvalIn.fetch_result('https://eval.in/1').url).to      eq 'https://eval.in/1.json'
-      expect(EvalIn.fetch_result('https://eval.in/1.json').url).to eq 'https://eval.in/1.json'
-    end
 
-    it 'can take a context for the user agent' do
-      stub_eval_in 'https://eval.in/1.json', user_agent: 'http://rubygems.org/gems/eval_in (c)'
-      expect(EvalIn.fetch_result('https://eval.in/1', context: 'c').url).to eq 'https://eval.in/1.json'
+    describe '.fetch_result' do
+      it 'fetches the json at the provided url and returns a result' do
+        url = 'https://eval.in/1.json'
+        stub_eval_in url
+        result = EvalIn.fetch_result url
+        assert_result result,
+                      exitstatus:        0,
+                      language:          ruby_result['lang'],
+                      language_friendly: ruby_result['lang_friendly'],
+                      code:              ruby_result['code'],
+                      output:            ruby_result['output'],
+                      status:            ruby_result['status'],
+                      url:               url
+      end
+
+      it 'jsonifies the url if it isn\'t already' do
+        stub_eval_in 'https://eval.in/1.json'
+        expect(EvalIn.fetch_result('https://eval.in/1').url).to      eq 'https://eval.in/1.json'
+        expect(EvalIn.fetch_result('https://eval.in/1.json').url).to eq 'https://eval.in/1.json'
+      end
+
+      it 'can take a context for the user agent' do
+        stub_eval_in 'https://eval.in/1.json', user_agent: 'http://rubygems.org/gems/eval_in (c)'
+        expect(EvalIn.fetch_result('https://eval.in/1', context: 'c').url).to eq 'https://eval.in/1.json'
+      end
+
     end
   end
 end
